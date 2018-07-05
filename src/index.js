23af026a9b2155e1ac3b6dc0fcf4eebfabb25ff6
Erin Black <erin.black@fieldnation.com>
2018-07-05 14:52:00 -0500

    created redux store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore} from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const storeInstance = createStore(
+    // This is a reducer
+    () => {
+        console.log(`Hey I'm a reducer`);
+    }
+);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
